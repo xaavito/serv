c91af2e77e06874bc1565c0a212be69a382c73fc
xaavito <jgonzalez@resolvit.com>
2019-03-27 11:54:18 -0300

    Conexion con la BD

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 var cors = require('cors');
 var app = express();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
 
 app.use(cors());
 
@@ -15,6 +21,19 @@ app.post('/api/greeting', (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.send(JSON.stringify({ a: 1 }));
+});
+
+app.get('/db', async (req, res) => {
+    try {
+        const client = await pool.connect()
+        const result = await client.query('SELECT * FROM partido');
+        const results = { 'results': (result) ? result.rows : null };
+        res.send(JSON.stringify(results));
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 app.listen(process.env.PORT || 5001, function () {
