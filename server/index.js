c7ef740b4bf6ae0c55e959512263dfc3807d3154
xaavito <jgonzalez@resolvit.com>
2019-03-29 11:11:21 -0300

    COmentado de codigo y mas metodos

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ var cors = require('cors');
 var app = express();
 var nodemailer = require('nodemailer');
 
+// CONFIGURACION DE CONEXION DE MAILS
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -13,6 +14,25 @@ var transporter = nodemailer.createTransport({
     }
 });
 
+// METODO QUE GENERA EL NUEVO PARTIDO, ARMA LA LISTA DE INVITADOS Y LAS INSERTA EN LA BD
+const generarNuevoPartido = (pool, fecha) => {
+    const client = await pool.connect()
+    const queryInsertarPartido = {
+        text: 'INSERT INTO partido (fecha, goles_blanco, goles_azul) values (to_date($1,\'DD/MM/YYYY\'),0,0)',
+        values: ['\'' + fecha + '\'']
+    }
+    await client.query(queryInsertarPartido);
+
+    const jugadores = await client.query('SELECT * FROM jugador');
+    jugadores.rows.forEach(function (jugador) {
+        console.log(jugador);
+        //ACA DEBERIAMOS HACER UN INSERT EN JUGADOR PARTIDO CON CADA UNO DE ESTOS.
+    });
+
+    client.release();
+}
+
+// ACCESO A LA BD
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -20,30 +40,37 @@ const pool = new Pool({
     ssl: true
 });
 
+// PARA EVITAR EL TEMA DE LA RESTRICCION DE DONDE LE PEGAN MUCHO BARDO CON LOCALHOST
 app.use(cors());
 
+// PINO LOGGER
+app.use(pino);
+
+// PARSEO MAGICO DE JSON DEL BODY
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
 
+// METODO BASURA PARA PROBAR
 app.get('/', function (req, res) {
     res.send('Bievenido al sistema de creacion y confirmacion de partidos de los miercoles');
 });
 
+// METODO BASURA PARA PROBAR
 app.post('/api/greeting', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.send(JSON.stringify({ a: 1 }));
 });
 
+// METODO ADMIN PARA GENERAR EL EVENTO
 app.post('/crear-partido', async (req, res) => {
     try {
-        console.log(req.body.fecha);
-        const client = await pool.connect()
-        const result = await client.query('INSERT INTO partido (fecha, goles_blanco, goles_azul) values (to_date(\'' + req.body.fecha + '\',\'DD/MM/YYYY\'),0,0)');
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Access-Control-Allow-Origin', '*');
-        client.release();
-        
+        //console.log(req.body.fecha);
+
+        this.generarNuevoPartido(pool, req.body.fecha);
+
         //move to cco??
         const mailOptions = {
             from: 'partidodelosmiercoles@gmail.com', // sender address
@@ -66,11 +93,15 @@ app.post('/crear-partido', async (req, res) => {
     }
 });
 
+// METODO BASURA
 app.get('/db', async (req, res) => {
     try {
         const client = await pool.connect()
         const result = await client.query('SELECT * FROM partido');
-        const results = { 'results': (result) ? result.rows : null };
+        result.rows.forEach(function (element) {
+            console.log(element);
+        });
+        //const results = { 'results': (result) ?  : null };
         res.send(JSON.stringify(results));
         client.release();
     } catch (err) {
