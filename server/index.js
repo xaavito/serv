1863070d336799264173bebf1f64e03fcba84986
xaavito <jgonzalez@resolvit.com>
2019-04-02 12:44:08 -0300

    Query fix

--- server/index.js
+++ server/index.js
@@ -89,7 +89,7 @@ const agregarNuevoInvitado = async (pool, invitado, transporter) => {
         console.log("AGREGAR INVITADO!");
 
         //BUSCO EL ID RECIEN INSERTADO DEL PARTIDO
-        const partido = await client.query('select max(id), fecha id_partido from partido');
+        const partido = await client.query('select max(id), fecha id_partido from partido group by fecha');
         const id_partido = partido.rows[0].id_partido;
         const fecha = partido.rows[0].fecha;
 
